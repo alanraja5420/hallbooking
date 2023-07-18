2aa2aa09236c46640af6f48fe58978b2df5cf472
alanraja5420 <138135481+alanraja5420@users.noreply.github.com>
2023-07-18 16:52:38 +0530

    login css

--- login/login.php
+++ login/login.php
@@ -5,18 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style1.css">
 </head>
 <body>
     <div class="container">
         <div class="left">
-            <div class="one">Welcome Page</div>       
+            <div class="one"></div>       
         </div>
         <div class="right">
-            <h1>Login Page</h1>
-            <input type="username" name="user" id="user" placeholder="username">
-            <input type="password" name="pass" id="pass" placeholder="password">
+        <img src="logo.png" alt="Girl in a jacket" width="85" height="120" >
+        <h2>St.Joseph's College</h2>
+        <h3>(Autonomous)</h3>
+        <h4>Tiruchirappalli - 02</h4>
+            <h1>Login Here</h1>
+            <input type="username" name="user" id="user" placeholder="Username">
+            <input type="password" name="pass" id="pass" placeholder="Password">
             <button class="continue">CONTINUE</button>
+            <h5> <link rel="pass" href=""> Forgot Password?</h5>
         </div>   
     </div>
 </body>
