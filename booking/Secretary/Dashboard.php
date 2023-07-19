53992492fc69b5a2689107e7696b9de7682573be
simonpeaky <139002746+simonpeaky@users.noreply.github.com>
2023-07-19 22:30:11 +0530

    secrtary

---
+++ booking/Secretary/Dashboard.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="logo"></div>
+        <ul class="menu">
+            <li class="active">
+                <a href="#" >
+                    <i class="fas fa-tachometer-alt"></i>
+                    <span> Dashboard </span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-user"></i>
+                    <span> Profile </span>
+                </a>
+            </li>
+            <li>
+                <a href="Halls.php">
+                    <i class="fas fa-chart-bar"></i>
+                    <span> Halls </span>
+                </a>
+            </li>
+            <li>
+                <a href="../booking/bookingform.php">
+                    <i class="fas fa-briefcase"></i>
+                    <span> Booking </span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-star"></i>
+                    <span> Status </span>
+                </a>
+            </li>
+            <li class="logout">
+                <a href="#">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <span> Logout </span>
+                </a>
+            </li>
+        </ul>
+        
+    </div>
+    <div class="main--content">
+       <div class="header--wrapper">
+       <div class="header--title">
+        <span> St.Joseph's college </span>
+        <h2> Dashboard </h2>
+       </div>
+       <div class="user--info">
+         <div class="search--box">
+           <i class="fa-solid fa-search"></i>
+           <input type="text" placeholder="search"/>
+         </div>
+         <img src="./sjc.png" alt=""/>
+       </div>
+       </div> 
+    </div>
+ </body>
+
+</html>
